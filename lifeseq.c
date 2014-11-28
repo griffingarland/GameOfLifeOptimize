6c32f1b83e5fb523e3eb488d7abbe820848364e8
Shamil Ishraq <shamil.ishraq@gmail.com>
2014-11-28 14:46:53 -0500

    trying to cleanup the code, but perf decreased

--- lifeseq.c
+++ lifeseq.c
@@ -11,14 +11,6 @@
 #include <stdio.h>
 
 #define NUM_THREADS 4
-
-typedef struct {
- int nrows;
- int ncols;
- int gens_max;
- int thread_id;
-} Args;
-
 /**
  * Swapping the two boards only involves swapping pointers, not
  * copying values.
@@ -31,67 +23,26 @@ typedef struct {
 
 #define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
 
-char* outboard; 
-char* inboard;
+typedef struct {
+// int nrows;
+// int ncols;
+ int thread_id;
+} Args;
 
-int LDA;
-
-pthread_t threads[NUM_THREADS];
-sem_t sem_start[NUM_THREADS];
-sem_t sem_finish[NUM_THREADS];
+char *outboard;
+char *inboard;
 
 int nrows_g, ncols_g;
 
-/*
 void golWorker(void * arg)
 {
-    Args *args = (Args *) arg; 
+    Args *args = (Args*) arg; 
+    //int nrows = args->nrows;; 
+    //int ncols = args->ncols; 
+    int nrows = nrows_g;
+    int ncols = ncols_g;
     int tid = args->thread_id;
-    int nrows = args->nrows;
-    int ncols = args->ncols;
-    int gen_max = args->gens_max;
-    int i,j;
-    int localgen;
-    int row_start = tid*nrows/NUM_THREADS;
-    int row_end = row_start + nrows/NUM_THREADS;
-
-    for (localgen = 0; localgen < gen_max; localgen++)
-    {
-        sem_wait(&sem_start[tid]);
-
-        for (i = row_start; i < row_end; i++)
-        {
-            for (j = 0; j < ncols; j++)
-            {
-                const int inorth = mod (i-1, nrows);
-                const int isouth = mod (i+1, nrows);
-                const int jwest = mod (j-1, ncols);
-                const int jeast = mod (j+1, ncols);
-
-                const char neighbor_count = 
-                    BOARD (inboard, inorth, jwest) + 
-                    BOARD (inboard, inorth, j) + 
-                    BOARD (inboard, inorth, jeast) + 
-                    BOARD (inboard, i, jwest) +
-                    BOARD (inboard, i, jeast) + 
-                    BOARD (inboard, isouth, jwest) +
-                    BOARD (inboard, isouth, j) + 
-                    BOARD (inboard, isouth, jeast);
-
-                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
-            }
-        }
-
-        sem_post(&sem_finish[tid]);
-    }
-    free(arg);
-} */
-
-void golWorker2(void * arg)
-{
-    int tid = *((int*)arg); 
-    int nrows = nrows_g; 
-    int ncols = ncols_g; 
+    int LDA = nrows;
     int i,j;
     int row_start = tid*nrows/NUM_THREADS;
     int row_end = row_start + nrows/NUM_THREADS;
@@ -155,49 +106,38 @@ sequential_game_of_life (char* outboard_,
         const int ncols,
         const int gens_max)
 {
-    nrows_g = nrows;
-    ncols_g = ncols;
-    outboard = outboard_;
-    inboard = inboard_;
-    
     /* HINT: in the parallel decomposition, LDA may not be equal to
        nrows! */
-    LDA = nrows;
     int i, j;
-    int arg0  = 0;
-    int arg1  = 1;
-    int arg2  = 2;
-    int arg3  = 3;
+    outboard = outboard_;
+    inboard = inboard_;
+    nrows_g = nrows;
+    ncols_g = ncols;
+    pthread_t threads[NUM_THREADS];
+    Args args[NUM_THREADS];
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+    //    args[i].nrows = nrows;
+    //    args[i].ncols = ncols;
+        args[i].thread_id = i;
+    }
 
     for(i = 0; i < gens_max; i++)
     {
-      /*
-      for(j = 0; j < NUM_THREADS; j++)
-      {
-          int *arg = (int*)malloc(sizeof(int));
-          *arg = j;
-          pthread_create(&threads[j], NULL, golWorker2,(void*) arg);
-      }*/
+        pthread_create(&threads[0], NULL, golWorker,(void*) &args[0]);
+        pthread_create(&threads[1], NULL, golWorker,(void*) &args[1]);
+        pthread_create(&threads[2], NULL, golWorker,(void*) &args[2]);
+        pthread_create(&threads[3], NULL, golWorker,(void*) &args[3]);
 
-          pthread_create(&threads[0], NULL, golWorker2,(void*) &arg0);
-          pthread_create(&threads[1], NULL, golWorker2,(void*) &arg1);
-          pthread_create(&threads[2], NULL, golWorker2,(void*) &arg2);
-          pthread_create(&threads[3], NULL, golWorker2,(void*) &arg3);
+        pthread_join(threads[0], NULL);
+        pthread_join(threads[1], NULL);
+        pthread_join(threads[2], NULL);
+        pthread_join(threads[3], NULL);
 
-      pthread_join(threads[0], NULL);
-      pthread_join(threads[1], NULL);
-      pthread_join(threads[2], NULL);
-      pthread_join(threads[3], NULL);
-
-      SWAP_BOARDS( outboard, inboard );
+        SWAP_BOARDS( outboard, inboard );
     }
 
-        /* 
-     * We return the output board, so that we know which one contains
-     * the final result (because we've been swapping boards around).
-     * Just be careful when you free() the two boards, so that you don't
-     * free the same one twice!!! 
-     */
     return inboard;
 }
 
