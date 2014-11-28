579f4183ad4732a90f0d2dc9cc80353f6e7f90c1
Shamil Ishraq <shamil.ishraq@gmail.com>
2014-11-28 01:07:25 -0500

    more improvements

--- lifeseq.c
+++ lifeseq.c
@@ -40,6 +40,8 @@ pthread_t threads[NUM_THREADS];
 sem_t sem_start[NUM_THREADS];
 sem_t sem_finish[NUM_THREADS];
 
+int nrows_g, ncols_g;
+
 void golWorker(void * arg)
 {
     Args *args = (Args *) arg; 
@@ -87,16 +89,12 @@ void golWorker(void * arg)
 
 void golWorker2(void * arg)
 {
-    Args *args = (Args *) arg; 
-    int tid = args->thread_id;
-    int nrows = args->nrows;
-    int ncols = args->ncols;
-    int gen_max = args->gens_max;
+    int tid = *((int*)arg); 
+    int nrows = nrows_g; 
+    int ncols = ncols_g; 
     int i,j;
-    int localgen;
     int row_start = tid*nrows/NUM_THREADS;
     int row_end = row_start + nrows/NUM_THREADS;
-
 
         for (i = row_start; i < row_end; i++)
         {
@@ -120,8 +118,7 @@ void golWorker2(void * arg)
                 BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
             }
         }
-
-    free(arg);
+        free(arg);
 }
 
 char*
@@ -131,61 +128,30 @@ sequential_game_of_life (char* outboard_,
         const int ncols,
         const int gens_max)
 {
+    nrows_g = nrows;
+    ncols_g = ncols;
     outboard = outboard_;
     inboard = inboard_;
     
     /* HINT: in the parallel decomposition, LDA may not be equal to
        nrows! */
     LDA = nrows;
-    int i;
-/*
-    for(i = 0; i < NUM_THREADS; i++)
-    {
-        Args *args = (Args*)malloc(sizeof(Args));
-        args->thread_id = i;
-        args->nrows = nrows;
-        args->ncols = ncols;
-        args->gens_max = gens_max;
-
-        pthread_create(&threads[i], NULL, golWorker,(void*) args);
-        sem_init(&sem_start[i], 0, 1);
-        sem_init(&sem_finish[i], 0, 0);
-    }
-*/
-    int j;
+    int i, j;
     for(i = 0; i < gens_max; i++)
     {
-      /*
-        sem_wait(&sem_finish[0]);
-        sem_wait(&sem_finish[1]);
-        sem_wait(&sem_finish[2]);
-        sem_wait(&sem_finish[3]);
+      for(j = 0; j < NUM_THREADS; j++)
+      {
+          int *arg = (int*)malloc(sizeof(int));
+          *arg = j;
+          pthread_create(&threads[j], NULL, golWorker2,(void*) arg);
+      }
 
-        SWAP_BOARDS( outboard, inboard );
-        
-        sem_post(&sem_start[0]);
-        sem_post(&sem_start[1]);
-        sem_post(&sem_start[2]);
-        sem_post(&sem_start[3]);
-        */
+      for (j = 0; j < NUM_THREADS; j++)
+      {
+        pthread_join(threads[j], NULL);
+      }
 
-    for(j = 0; j < NUM_THREADS; j++)
-    {
-        Args *args = (Args*)malloc(sizeof(Args));
-        args->thread_id = j;
-        args->nrows = nrows;
-        args->ncols = ncols;
-        args->gens_max = gens_max;
-
-        pthread_create(&threads[j], NULL, golWorker2,(void*) args);
-    }
-
-    for (j = 0; j < NUM_THREADS; j++)
-    {
-      pthread_join(threads[j], NULL);
-    }
-
-        SWAP_BOARDS( outboard, inboard );
+      SWAP_BOARDS( outboard, inboard );
     }
 
         /* 
